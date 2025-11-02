1080bb2bd890e5c46f59978069e0f337cf44de0d
abhijithjoshy <abhijithjoshy500@gmail.com>
2025-11-02 20:55:23 +0530

    fix:appname change

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'User Management'),
+    'name' => 'User Management',
 
     /*
     |--------------------------------------------------------------------------
